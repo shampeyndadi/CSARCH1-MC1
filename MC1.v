5cec4327362798f62d9b857ec167f27dcce85d9c
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-02-16 23:14:25 +0800

    changed negation sign

--- MC1.v
+++ MC1.v
@@ -7,12 +7,12 @@ module MC1(F, A, B, C, D);
     wire w1,w2,w3,w4, w5, w6, w7;
 
     or G1(w1, A, B, C, D);
-    or G2(w2, A, B, C, !D);
-    or G3(w3, A, B, !C, D);
-    or G4(w4, !A, B, C, D);
-    or G5(w5, !A, B, !C, D);
-    or G6(w6, !A, !B, C, D);
-    or G7(w7, !A, !B, !C, D);
+    or G2(w2, A, B, C, ~D);
+    or G3(w3, A, B, ~C, D);
+    or G4(w4, ~A, B, C, D);
+    or G5(w5, ~A, B, ~C, D);
+    or G6(w6, ~A, ~B, C, D);
+    or G7(w7, ~A, ~B, ~C, D);
 
     and G8(F, w1, w2, w3, w4, w5, w6, w7);
     
