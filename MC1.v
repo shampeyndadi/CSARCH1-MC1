474acf462bcee3a26ae72ce10f9d764b481cee44
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-02-16 19:53:22 +0800

    not sure what im doing

--- MC1.v
+++ MC1.v
@@ -1,3 +1,18 @@
 module MC1(F, A, B, C, D);
-    hello
+    input A,B,C,D;
+    output F;
+    wire w1,w2,w3,w4, w5, w6, w7;
+
+    or (w1, A, B, C, D);
+    or (w2, A, B, C, ~D);
+    or (w3, A, B, ~C, D);
+    or (w4, ~A, B, C, D);
+    or (w5, ~A, B, ~C, D);
+    or (w6, ~A, ~B, C, D);
+    or (w7, ~A, ~B, ~C, D);
+
+    and (w1, w2, w3, w4, w5, w6, w7);
+
+
+
 endmodule
