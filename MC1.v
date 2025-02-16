51ddc54075ca139a27b59e59953b6cd10385b053
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-02-16 20:13:18 +0800

    fixed

--- MC1.v
+++ MC1.v
@@ -1,17 +1,22 @@
+'`timescale 1ps/1ps
+
 module MC1(F, A, B, C, D);
 
     input A,B,C,D;
     output F;
     wire w1,w2,w3,w4, w5, w6, w7;
 
-    or (w1, A, B, C, D);
-    or (w2, A, B, C, ~D);
-    or (w3, A, B, ~C, D);
-    or (w4, ~A, B, C, D);
-    or (w5, ~A, B, ~C, D);
-    or (w6, ~A, ~B, C, D);
-    or (w7, ~A, ~B, ~C, D);
+    or G1(w1, A, B, C, D);
+    or G2(w2, A, B, C, ~D);
+    or G3(w3, A, B, ~C, D);
+    or G4(w4, ~A, B, C, D);
+    or G5(w5, ~A, B, ~C, D);
+    or G6(w6, ~A, ~B, C, D);
+    or G7(w7, ~A, ~B, ~C, D);
 
-    and (w1, w2, w3, w4, w5, w6, w7);
+    and G8(F, w1, w2, w3, w4, w5, w6, w7);
+
+
+
 
 endmodule
